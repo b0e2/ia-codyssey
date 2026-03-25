983d57be10c967fe51638192a7baef427ee72772
b0e2 <beenjeong02@gmail.com>
2026-03-25 18:07:44 +0900

    feat: 인화성 0.7 이상 목록 출력 및 Mars_Base_Inventory_danger.csv 파일로 저장

--- step1_essential/course1/prob3_find_flammables/main.py
+++ step1_essential/course1/prob3_find_flammables/main.py
@@ -1,14 +1,18 @@
 # 문제3 인화 물질을 찾아라
 # Python Version: 3.9.6 
 
-# 화성기지 입고 물질 목록 원본 데이터 
+# 원본 데이터 파일 (화성기지 입고 물질 목록 CSV 파일)
 CSV_FILE_PATH = 'Mars_Base_Inventory_List.csv'
+# 필터링 데이터 파일 (인화성 지수 0.7 이상인 물질 목록 CSV 파일)
+CSV_DANGER_FILE_PATH = 'Mars_Base_Inventory_danger.csv'
 
 
 def find_flammables(target_path):
-    # 입고 물질 목록 리스트 
-    inventory_list = []
+    # 입고 물질 헤더, 목록, 위험 물질 리스트 
     header = []
+    inventory_list = []
+    inventory_danger_list = []
+    
     try:
         # 파일 열기 및 입고 물질 목록 읽기
         with open(target_path, 'r', encoding='utf-8') as file:
@@ -25,7 +29,21 @@ def find_flammables(target_path):
             
         # [수행과제] 인화성 높은 순 정렬 
         inventory_list.sort(key=lambda x: float(x[-1]), reverse=True)
+                
+        # 인화성 지수 0.7 이상인 물질 필터링
+        inventory_danger_list = [item for item in inventory_list if float(item[-1]) >= 0.7]
+                
+        # [수행과제] 인화성 지수가 0.7 이상인 물질 출력        
+        print(f'\n--- [인화성 지수 0.7 이상인 물질 목록 {len(inventory_danger_list)} lines] ---')
+        for item in [header] + inventory_danger_list:
+            print(','.join(item))
         
+        # 스크립트 위치 기준의 절대 경로 생성 
+        inventory_danger_list_path = __file__.replace('main.py', CSV_DANGER_FILE_PATH)
+        
+        # [수행과제] 인화성 지수가 0.7 이상인 물질 목록을 별도의 CSV 파일로 저장 (utf-8 인코딩)    
+        with open(inventory_danger_list_path, 'w', encoding='utf-8') as f:
+            f.writelines([','.join(item) + '\n' for item in [header] + inventory_danger_list]   )
         
         
     except FileNotFoundError:
