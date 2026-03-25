a19d696071bc48ce7d65e74bfe74f9f96cfd485b
b0e2 <beenjeong02@gmail.com>
2026-03-25 18:36:15 +0900

    feat: 인화성 순서로 정렬된 리스트 이진 파일 저장 및 출력

--- step1_essential/course1/prob3_find_flammables/main.py
+++ step1_essential/course1/prob3_find_flammables/main.py
@@ -5,6 +5,8 @@
 CSV_FILE_PATH = 'Mars_Base_Inventory_List.csv'
 # 필터링 데이터 파일 (인화성 지수 0.7 이상인 물질 목록 CSV 파일)
 CSV_DANGER_FILE_PATH = 'Mars_Base_Inventory_danger.csv'
+# 이진 데이터 파일 (인화성 순서로 정렬된 배열의 내용을 이진 파일 형태로 저장)
+BIN_FILE_PATH = 'Mars_Base_Inventory_List.bin'
 
 
 def get_absolute_path(filename):
@@ -41,6 +43,24 @@ def save_inventory_csv(target_path, header, data_list):
     # [수행과제] 인화성 지수가 0.7 이상인 물질 목록을 별도의 CSV 파일로 저장 (utf-8 인코딩)    
     with open(target_path, 'w', encoding='utf-8') as f:
         f.writelines([','.join(item) + '\n' for item in [header] + data_list])
+        
+        
+def save_inventory_binary(target_path, header, data_list):
+    """[보너스 과제] 정렬된 배열의 내용을 이진 파일 형태로 저장합니다."""
+    with open(target_path, 'wb') as f:
+        for item in [header] + data_list:
+            # 문자열로 합친 뒤 utf-8 바이트로 인코딩(encode)하여 저장
+            line_data = ','.join(item) + '\n'
+            f.write(line_data.encode('utf-8'))
+
+
+def load_and_print_binary(target_path):
+    """[보너스 과제] 저장된 이진 파일의 내용을 다시 읽어 들여서 화면에 출력합니다."""
+    print('\n---[인화성 순서로 정렬된 이진 파일] ---')
+    with open(target_path, 'rb') as f:
+        for line in f:
+            # 바이트 데이터를 다시 문자열로 디코딩(decode)하여 출력
+            print(line.decode('utf-8').strip())       
 
 
 def find_flammables(target_path):
@@ -63,9 +83,16 @@ def find_flammables(target_path):
         
         # 스크립트 위치 기준의 절대 경로 생성 
         inventory_danger_list_path = get_absolute_path(CSV_DANGER_FILE_PATH)
+        inventory_bin_path = get_absolute_path(BIN_FILE_PATH)
         
         # 파일 저장 호출
         save_inventory_csv(inventory_danger_list_path, header, inventory_danger_list)
+        
+        # [보너스 과제] 인화성 순서로 정렬된 배열의 내용을 이진 파일 형태로 저장
+        save_inventory_binary(inventory_bin_path, header, inventory_list)
+        
+        # [보너스 과제] 저장된 이진 파일의 내용을 다시 읽어 들여서 화면에 출력
+        load_and_print_binary(inventory_bin_path)
         
     except FileNotFoundError:
         print(f'Error: 파일 "{target_path}"을(를) 찾을 수 없습니다.')
