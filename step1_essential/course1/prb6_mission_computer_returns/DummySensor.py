2825376b7c2b41a8893628206acd61f8cd2e42ec
b0e2 <beenjeong02@gmail.com>
2026-04-02 02:23:59 +0900

    chore: 문제6 파일 구성 및 기초 구조 설계

---
+++ step1_essential/course1/prb6_mission_computer_returns/DummySensor.py
@@ -0,0 +1,20 @@
+# DummySensor.py
+# 문제6 미션 컴퓨터 리턴즈 
+# Python Version: 3.9.6
+
+import random as rnd
+
+class DummySensor:
+    
+    def __init__(self) -> None:
+        pass
+    
+    def set_env(self) -> None:
+        pass
+    
+    def get_env(self) -> dict:
+        pass
+    
+    
+if __name__ == '__main__':
+    ds = DummySensor()
