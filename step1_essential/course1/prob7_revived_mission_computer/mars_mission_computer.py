3a24dfb795a28339b5c41e728433928519b43831
b0e2 <beenjeong02@gmail.com>
2026-04-09 11:38:30 +0900

    chore: 문제7 파일 구성 및 구조 설계

---
+++ step1_essential/course1/prob7_revived_mission_computer/mars_mission_computer.py
@@ -0,0 +1,20 @@
+# mars_mission_computer.py
+# 문제7 살아난 미션 컴퓨터
+# Python Version: 3.9.6
+
+class MissionComuter:
+    
+    def __init__(self) -> None:
+        pass
+        
+    def set_env(self) -> None:
+        pass
+    
+    def get_sensor_data(self) -> dict:
+        pass 
+
+
+if __name__ == '__main__':
+    ds = MissionComuter()
+    RunComputer = MissionComuter()
+    RunComputer.get_sensor_data()
