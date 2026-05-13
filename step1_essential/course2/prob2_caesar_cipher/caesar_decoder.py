c8a10b51aa32ca7dd02b545159ce6fea5473a576
b0e2 <beenjeong02@gmail.com>
2026-05-13 18:41:06 +0900

    chore: 과정2 문제2 파일 구성 및 구조 설계

---
+++ step1_essential/course2/prob2_caesar_cipher/caesar_decoder.py
@@ -0,0 +1,15 @@
+
+def read_password_file(file_path: str) -> str:
+    pass
+
+def caesar_cipher_decode(target_text: str) -> dict:
+    pass 
+
+def save_result_file(file_path: str, decoded_text: str) -> bool:
+    pass
+
+def main() -> None:
+    pass
+
+if __name__ == '__main__':
+    main()
